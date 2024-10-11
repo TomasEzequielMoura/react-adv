73a3266ffbfebb3f5a7923aa695a07bc687441c9
TomasEzequielMoura <tomasemoura@gmail.com>
2024-10-11 18:52:43 -0300

    aclaracion de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { Navigation } from './routes/Navigation';
+import { Navigation } from "./routes/Navigation";
+
+// En esta sección vamos a llevar nuestro sistema de rutas a otro nivel, el objetivo principal sera:
+
+// Aplicar Lazy Load en cada Componente
+
+// Aplicar Lazy Load por módulo
+
+// La idea del módulo es que nos permita cargarlo y todas sus dependencias en conjunto.
 
 function App() {
   return (
